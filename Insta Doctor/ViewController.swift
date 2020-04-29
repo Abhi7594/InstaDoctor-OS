064634d6c6f568430c046d964f2d7021249160a2
Abhishek Rathod <abhishek@softgates.ae>
2020-04-29 21:41:52 +0530

    Initial Commit

---
+++ Insta Doctor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Insta Doctor
+//
+//  Created by Abhishek Rathod on 29/04/20.
+//  Copyright © 2020 Aximo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Insta DoctorTests/Insta_DoctorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Insta_DoctorTests.swift
+//  Insta DoctorTests
+//
+//  Created by Abhishek Rathod on 29/04/20.
+//  Copyright © 2020 Aximo. All rights reserved.
+//
+
+import XCTest
+@testable import Insta_Doctor
+
+class Insta_DoctorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
